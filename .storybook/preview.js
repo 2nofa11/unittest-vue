b6a697a2e8aaf0aa5ce640cebd2a60c8bcd840ad
2nofa11 <agsh0505@gmail.com>
2025-04-26 20:49:30 +0900

    Storybookの設定から不要なアクション関連のコードを削除し、推奨に従った更新を行いました。

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,5 +1,4 @@
 export const parameters = {
-  // actions: { argTypesRegex: "^on[A-Z].*" }, // Removed based on Storybook 8 recommendation
   controls: {
     matchers: {
       color: /(background|color)$/i,
